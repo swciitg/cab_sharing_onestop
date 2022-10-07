1f8db25482a824844ab37b0088e62d4d38f38c4f
Tanisha <tanishasalvi12@gmail.com>
2022-10-07 15:59:02 +0530

    Design Signup/Signin Screens

--- lib/screens/auth/signin.dart
+++ lib/screens/auth/signin.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SigninScreen extends StatefulWidget {
   static const id = "/signin";
@@ -20,110 +21,165 @@ class _SigninScreenState extends State<SigninScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login Screen"),
-      ),
+      backgroundColor: Color.fromRGBO(27, 27, 29, 1),
+      resizeToAvoidBottomInset: false,
       body: Form(
         key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "SignIn Screen",
-              style: TextStyle(
-                  fontSize: 20
+        child: SingleChildScrollView(
+          reverse: true,
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(height: 200,),
+              Icon(
+                Icons.directions_car,
+                color: Colors.white60,
+                size: 100,
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
-              child: TextFormField(
-                onChanged: (value){
-                  email=value.trim();
-                },
-                validator: (value){
-                  if (value == null) {
-                    return 'Please enter your email';
-                  }
-                  value=value.trim();
-                  if(value.isEmpty){
-                    return 'Please enter your email';
-                  }
-                  return null;
-                },
-                decoration: const InputDecoration(
-                    hintText: "Enter Email"
+              SizedBox(height: 40,),
+              Container(
+                margin: EdgeInsets.only(left: 30, right: 30),
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(39, 49, 65, 1),
+                    borderRadius: BorderRadius.all(
+                        Radius.circular(21)
+                    )
                 ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
-              child: TextFormField(
-                onChanged: (value){
-                  password=value.trim();
-                },
-                validator: (value){
-                  if (value == null) {
-                    return 'Please enter a password';
-                  }
-                  value=value.trim();
-                  if(value.isEmpty){
-                    return 'Please enter a password';
-                  }
-                  return null;
-                },
-                maxLength: 12,
-                decoration: const InputDecoration(
-                    hintText: "Enter Password"
-                ),
-              ),
-            ),
-            Builder(builder: (builderContext) => GestureDetector(
-              onTap: () async {
-                try{
-                  bool validate = _formKey.currentState!.validate();
-                  if(validate){
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
-                    DocumentSnapshot userSnapshot = await FirebaseFirestore.instance.collection('users').doc(email).get();
-                    context.read<LoginStore>().saveUserData(userSnapshot["name"], email, userSnapshot["phonenumber"]);
-                    Navigator.of(context).pushReplacementNamed('/home');
-                  }
-                }
-                catch (err){
-                  Scaffold.of(builderContext).showSnackBar(SnackBar(content: Text(err.toString())));
-                }
-              },
-              child: Container(
-                color: Colors.amber,
-                padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 6),
-                child: const Text(
-                  "Proceed",
-                  style: TextStyle(
-                      fontSize: 16
+                padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                child: TextFormField(
+                  style: GoogleFonts.montserrat(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 0.5,
+                    color: const Color(0xFFBDC7DC),
+                  ),
+                  onChanged: (value){
+                    email=value.trim();
+                  },
+                  validator: (value){
+                    if (value == null) {
+                      return 'Please enter your email';
+                    }
+                    value=value.trim();
+                    if(value.isEmpty){
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "Enter Email",
+                    hintStyle: TextStyle(
+                      color: Colors.white60,
+                    ),
                   ),
                 ),
               ),
-            )),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text(
-                  "No Account ?",
-                  style: TextStyle(
-                      fontSize: 16
+              SizedBox(height: 30,),
+              Container(
+                margin: EdgeInsets.only(left: 30, right: 30),
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(39, 49, 65, 1),
+                    borderRadius: BorderRadius.all(
+                        Radius.circular(21)
+                    )
+                ),
+                padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                child: TextFormField(
+                  style: GoogleFonts.montserrat(
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 0.5,
+                    color: const Color(0xFFBDC7DC),
+                  ),
+                  obscureText: true,
+                  onChanged: (value){
+                    password=value.trim();
+                  },
+                  validator: (value){
+                    if (value == null) {
+                      return 'Please enter a password';
+                    }
+                    value=value.trim();
+                    if(value.isEmpty){
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                  maxLength: 12,
+                  decoration: const InputDecoration(
+                      hintText: "Enter Password",
+                      hintStyle: TextStyle(
+                        color: Colors.white60,
+                      )
                   ),
                 ),
-                TextButton(
-                    onPressed: (){
-                      Navigator.of(context).pushReplacementNamed('/signup');
-                    }, child: const Text(
-                  "SignUp",
-                  style: TextStyle(
-                      fontSize: 16
+              ),
+              SizedBox(height: 40,),
+              Builder(builder: (builderContext) => GestureDetector(
+                onTap: () async {
+                  try{
+                    bool validate = _formKey.currentState!.validate();
+                    if(validate){
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+                      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance.collection('users').doc(email).get();
+                      context.read<LoginStore>().saveUserData(userSnapshot["name"], email, userSnapshot["phonenumber"]);
+                      Navigator.of(context).pushReplacementNamed('/home');
+                    }
+                  }
+                  catch (err){
+                    Scaffold.of(builderContext).showSnackBar(SnackBar(content: Text(err.toString())));
+                  }
+                },
+                child: Container(
+                  height: 50,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(118, 172, 255, 1),
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(16)
+                      )
                   ),
-                ))
-              ],
-            )
-          ],
+                  padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 15),
+                  child: Text(
+                    "Sign In",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.montserrat(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.5,
+                    ),
+                  ),
+                ),
+              )),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "No Account ?",
+                    style: GoogleFonts.montserrat(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.5,
+                      color: const Color(0xFFBDC7DC),
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: (){
+                        Navigator.of(context).pushReplacementNamed('/signup');
+                      }, child: Text(
+                    "SignUp",
+                    style: GoogleFonts.montserrat(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 0.5,
+                      color:Color.fromRGBO(118, 172, 255, 1),
+                    ),
+                  ))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
