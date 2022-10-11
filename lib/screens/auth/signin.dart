ea8015024ca298d85d5ba77baac2ea5bc5888d82
StrawHATDEEPAK <drai4778098@gmail.com>
2022-10-11 22:54:32 +0530

    Refactored decoration and styles

--- lib/screens/auth/signin.dart
+++ lib/screens/auth/signin.dart
@@ -1,9 +1,9 @@
+import 'package:campus_ola/decorations/sign_in_style.dart';
 import 'package:campus_ola/stores/login_store.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class SignInScreen extends StatefulWidget {
   static const id = "/signin";
@@ -30,155 +30,128 @@ class _SignInScreenState extends State<SignInScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              const SizedBox(height: 200,),
+              const SizedBox(
+                height: 200,
+              ),
               const Icon(
                 Icons.directions_car,
                 color: Colors.white60,
                 size: 100,
               ),
-              const SizedBox(height: 40,),
+              const SizedBox(
+                height: 40,
+              ),
               Container(
                 margin: const EdgeInsets.only(left: 30, right: 30),
-                decoration: const BoxDecoration(
-                    color: Color.fromRGBO(39, 49, 65, 1),
-                    borderRadius: BorderRadius.all(
-                        Radius.circular(21)
-                    )
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                decoration: kTextFieldFormContainerDecoration,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                 child: TextFormField(
-                  style: GoogleFonts.montserrat(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0.5,
-                    color: const Color(0xFFBDC7DC),
-                  ),
-                  onChanged: (value){
-                    email=value.trim();
+                  style: kTextFieldFormStyle,
+                  onChanged: (value) {
+                    email = value.trim();
                   },
-                  validator: (value){
+                  validator: (value) {
                     if (value == null) {
                       return 'Please enter your email';
                     }
-                    value=value.trim();
-                    if(value.isEmpty){
+                    value = value.trim();
+                    if (value.isEmpty) {
                       return 'Please enter your email';
                     }
                     return null;
                   },
-                  decoration: const InputDecoration(
-                    hintText: "Enter Email",
-                    hintStyle: TextStyle(
-                      color: Colors.white60,
-                    ),
-                  ),
+                  decoration: kTextFieldFormDecoration,
                 ),
               ),
-              const SizedBox(height: 30,),
+              const SizedBox(
+                height: 30,
+              ),
               Container(
                 margin: const EdgeInsets.only(left: 30, right: 30),
-                decoration: const BoxDecoration(
-                    color: Color.fromRGBO(39, 49, 65, 1),
-                    borderRadius: BorderRadius.all(
-                        Radius.circular(21)
-                    )
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                decoration: kTextFieldFormContainerDecoration,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
                 child: TextFormField(
-                  style: GoogleFonts.montserrat(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 0.5,
-                    color: const Color(0xFFBDC7DC),
-                  ),
+                  style: kTextFieldFormStyle,
                   obscureText: true,
-                  onChanged: (value){
-                    password=value.trim();
+                  onChanged: (value) {
+                    password = value.trim();
                   },
-                  validator: (value){
+                  validator: (value) {
                     if (value == null) {
                       return 'Please enter a password';
                     }
-                    value=value.trim();
-                    if(value.isEmpty){
+                    value = value.trim();
+                    if (value.isEmpty) {
                       return 'Please enter a password';
                     }
                     return null;
                   },
                   maxLength: 12,
-                  decoration: const InputDecoration(
-                      hintText: "Enter Password",
-                      hintStyle: TextStyle(
-                        color: Colors.white60,
-                      )
-                  ),
+                  decoration: kTextFieldFormDecoration,
                 ),
               ),
-              const SizedBox(height: 40,),
-              Builder(builder: (builderContext) => GestureDetector(
-                onTap: () async {
-                  try{
-                    bool validate = _formKey.currentState!.validate();
-                    if(validate){
-                      await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
-                      DocumentSnapshot userSnapshot = await FirebaseFirestore.instance.collection('users').doc(email).get();
-                      if(!mounted)
-                        {
+              const SizedBox(
+                height: 40,
+              ),
+              Builder(
+                builder: (builderContext) => GestureDetector(
+                  onTap: () async {
+                    try {
+                      bool validate = _formKey.currentState!.validate();
+                      if (validate) {
+                        await FirebaseAuth.instance.signInWithEmailAndPassword(
+                            email: email, password: password);
+                        DocumentSnapshot userSnapshot = await FirebaseFirestore
+                            .instance
+                            .collection('users')
+                            .doc(email)
+                            .get();
+                        if (!mounted) {
                           return;
                         }
-                      context.read<LoginStore>().saveUserData(userSnapshot["name"], email, userSnapshot["phonenumber"]);
-                      Navigator.of(context).pushReplacementNamed('/home');
+                        context.read<LoginStore>().saveUserData(
+                            userSnapshot["name"],
+                            email,
+                            userSnapshot["phonenumber"]);
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      }
+                    } catch (err) {
+                      ScaffoldMessenger.of(builderContext).showSnackBar(
+                          SnackBar(content: Text(err.toString())));
                     }
-                  }
-                  catch (err){
-                    ScaffoldMessenger.of(builderContext).showSnackBar(SnackBar(content: Text(err.toString())));
-                  }
-                },
-                child: Container(
-                  height: 50,
-                  width: 150,
-                  decoration: const BoxDecoration(
-                      color: Color.fromRGBO(118, 172, 255, 1),
-                      borderRadius: BorderRadius.all(
-                          Radius.circular(16)
-                      )
-                  ),
-                  padding: const EdgeInsets.symmetric(horizontal: 12,vertical: 15),
-                  child: Text(
-                    "Sign In",
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.montserrat(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.5,
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 150,
+                    decoration: kSignInContainerDecoration,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 15),
+                    child: Text(
+                      "Sign In",
+                      textAlign: TextAlign.center,
+                      style: kSignInTextStyle,
                     ),
                   ),
                 ),
-              )),
+              ),
               Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   Text(
                     "No Account ?",
-                    style: GoogleFonts.montserrat(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.5,
-                      color: const Color(0xFFBDC7DC),
-                    ),
+                    style: kNoAccountTextStyle,
                   ),
                   TextButton(
-                      onPressed: (){
-                        Navigator.of(context).pushReplacementNamed('/signup');
-                      }, child: Text(
-                    "SignUp",
-                    style: GoogleFonts.montserrat(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w500,
-                      letterSpacing: 0.5,
-                      color:const Color.fromRGBO(118, 172, 255, 1),
+                    onPressed: () {
+                      Navigator.of(context).pushReplacementNamed('/signup');
+                    },
+                    child: Text(
+                      "SignUp",
+                      style: kSignUpTextStyle,
                     ),
-                  ))
+                  )
                 ],
               )
             ],
