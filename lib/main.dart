dc58529714c3ac7a786d98a85899c4fa09f9cdca
Vignesh Rao <vigneshravirao@gmail.com>
2022-12-13 20:59:36 +0530

    Add main.dart

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:cab_sharing/cab_sharing.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    String key = const String.fromEnvironment('SECURITY_KEY');
+    return MaterialApp(
+      home: CabSharingScreen(userData: {
+        'name': 'Me',
+        'email': 'me@iitg.ac.in',
+        'security-key': key
+      }),
+    );
+  }
+}
