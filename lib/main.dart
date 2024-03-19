25492a5464096973cc97e666c3b5959842186d8b
Arpit Sureka <hardikroongta8@gmail.com>
2024-03-20 00:00:58 +0530

    updated kotlin version

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    String key = const String.fromEnvironment('SECURITY_KEY');
     return MaterialApp(
       home: CabSharingSplashScreen()
     );
