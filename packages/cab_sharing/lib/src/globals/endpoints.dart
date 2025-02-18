4cc360510cce9ba06383546c2f405466f31c9eed
swciitg <swciitghy@gmail.com>
2025-02-18 20:43:08 +0530

    Migration to .env files

--- packages/cab_sharing/lib/src/globals/endpoints.dart
+++ packages/cab_sharing/lib/src/globals/endpoints.dart
@@ -1,12 +1,15 @@
 class Endpoints {
-  static const baseUrl = String.fromEnvironment('SERVER-URL');
-  static const apiSecurityKey = String.fromEnvironment('SECURITY-KEY');
+  static const baseUrl = String.fromEnvironment('SERVER_URL');
+  static const apiSecurityKey = String.fromEnvironment('SECURITY_KEY');
   static const cabSharingURL = "/campus-travel";
   static const cabSharingMyAdsURL = "/campus-travel/myads";
   static const cabSharingChatURL = "/campus-travel/chat";
   static const cabSharingAllURL = "/campus-travel/all";
 
   static getHeader() {
-    return {'Content-Type': 'application/json', 'security-key': Endpoints.apiSecurityKey};
+    return {
+      'Content-Type': 'application/json',
+      'security-key': Endpoints.apiSecurityKey
+    };
   }
 }
