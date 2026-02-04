dd0d106130fa11489253d92d666fa70c2ea393cf
div1681 <g.divyansh@iitg.ac.in>
2026-02-04 18:15:00 +0530

    Basic UI build successful

--- packages/cab_sharing/lib/src/widgets/home/home_date_picker_modal.dart
+++ packages/cab_sharing/lib/src/widgets/home/home_date_picker_modal.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+//import 'package:cab_sharing/src/widgets/ui/date_time.dart' as cab_sharing_ui;
+import 'package:onestop_ui/widget_demo/dates.dart' ;
+
+
 import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
 import 'package:onestop_ui/index.dart';
 
@@ -50,12 +54,14 @@ class HomeDatePickerModal extends StatelessWidget {
           SizedBox(
             height: 320,
             child: SingleChildScrollView(
-              child: OCalendar(
-                dateSelected: (date) {
-                  onDateSelected(date);
-                  Navigator.pop(context);
-                },
-              ),
+              child: 
+              SizedBox(height:400, child: Text("CALENDAR"),)
+              // OCalendar(
+              //   dateSelected: (date) {
+              //     onDateSelected(date);
+              //     Navigator.pop(context);
+              //   },
+              // ),
             ),
           ),
         ],
